ab90fa71cf4b53a80ad5ed2c47cc96d5cea8214d
Jacob Brady <jacob@jdbrady.info>
2019-10-15 17:23:45 -0400

    feat: box page logo links to login

--- src/components/BoxPage.js
+++ src/components/BoxPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './BoxPage.scss';
 import logo from '../images/bctc-tas-simple-logo.svg';
 
 const BoxPage = ({ afterBox: AfterBox, children }) => (
   <div className="box-page">
-    <img className="box-page__logo" src={logo} alt="BCTC TAS"/>
+    <Link to="/login" style={{ margin: '2rem 1rem' }}>
+      <img className="box-page__logo" src={logo} alt="BCTC TAS"/>
+    </Link>
     <div className="box-page__box">{children}</div>
     {AfterBox && (
       <div className="box-page__after-box">
