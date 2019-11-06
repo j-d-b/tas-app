4b4c7498e640510903979e3815a321442c8ca22b
Jacob Brady <jacob@jdbrady.info>
2019-11-06 17:57:40 -0500

    feat: AdminOnlyConfiguration

--- src/containers/Configuration.js
+++ src/containers/Configuration.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Configuration = () => <div>CONFIGURATION</div>;
+import AdminOnlyConfiguration from './AdminOnlyConfiguration';
+
+const Configuration = () => (
+  <div style={{ maxWidth: 500, margin: '2rem' }}>
+    <h1>Configuration</h1>
+    <AdminOnlyConfiguration />
+  </div>
+);
 
 export default Configuration;
