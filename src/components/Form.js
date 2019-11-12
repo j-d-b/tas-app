647014589fb2c96a8fa51fb1eb5178875465acf5
Jacob Brady <jacob@jdbrady.info>
2019-11-12 09:25:25 -0500

    feat: FormInput forward ref

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,7 +9,7 @@ export const FormButton = ({ variety, children, className, ...props }) => {
 
 export const FormGroup = ({ children }) => <div className="form-group">{children}</div>;
 
-export const FormInput = props => <input className="form-input" {...props} />;
+export const FormInput = React.forwardRef((props, ref) => <input className="form-input" ref={ref} {...props} />);
 
 export const FormSelect = ({ options, name, value, placeholder, disabled, ...rest}) => (
   <select className="form-select" style={{ color: !value ? 'lightgray' : (disabled ? 'graytext' : 'black') }} value={value} disabled={disabled} {...rest}>
