9cfa574a9d63c6fc949d22a7d0627ded2bdce174
Jacob Brady <jacob@jdbrady.info>
2019-11-14 09:57:57 -0500

    feat: ResponseMessage more prop options

--- src/components/ResponseMessage.js
+++ src/components/ResponseMessage.js
@@ -4,9 +4,10 @@ import './ResponseMessage.scss';
 import { formatError } from '../utils';
 
 export const SuccessMessage = ({ children }) => (
-  <div style={{ color: 'green', marginTop: '0.5rem', fontSize: '0.9rem' }}>{children}</div>
+  <div style={{ color: 'green', marginTop: '0.5rem', fontSize: '0.9rem' }}>{children || 'Saved successfully'}</div>
 );
 
-export const ErrorMessage = ({ error }) => (
-  <div style={{ color: 'red', marginTop: '0.5rem', fontSize: '0.9rem' }}>{formatError(error)}</div>
-);
+export const ErrorMessage = ({ error, children }) => {
+  if (error) return <div style={{ color: 'red', marginTop: '0.5rem', fontSize: '0.9rem' }}>{formatError(error)}</div>
+  return <div style={{ color: 'red', marginTop: '0.5rem', fontSize: '0.9rem' }}>{children}</div>
+};
