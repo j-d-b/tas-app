1a24abed3869985ce6f2c3d85f6546d28fa464ae
Jacob Brady <jacob@jdbrady.info>
2019-11-15 11:01:46 -0500

    fix/feat: mobile navbar disable scroll on body when open, fixes page-wrapper height issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import AnimateHeight from 'react-animate-height';
 
 import './Navbar.scss';
@@ -7,9 +8,16 @@ import LogoutButton from './LogoutButton';
 import logo from '../images/bctc-tas-simple-logo.svg';
 import { ReactComponent as BarsIcon } from '../images/bars-solid.svg';
  
-
 const Navbar = ({ navLinks }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.setAttribute('class', 'disable-body-scroll');
+    } else {
+      document.body.removeAttribute('class');
+    }
+  }, [isMenuOpen]);
 
   return (
     <div>
@@ -44,6 +52,10 @@ const Navbar = ({ navLinks }) => {
           <LogoutButton className="navbar__link navbar__link--mobile navbar__link--mobile--logout-button" />
         </div>
       </AnimateHeight>
+
+      <CSSTransition in={isMenuOpen} classNames="mobile-menu-backdrop" timeout={300} unmountOnExit>
+        <div className="mobile-menu-backdrop" onClick={() => setIsMenuOpen(false)}></div>
+      </CSSTransition>
     </div>
   );
 };
