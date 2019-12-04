8afd2afee8d89031ee5f9d903ff1bf693aa35dc5
Jacob Brady <jacob@jdbrady.info>
2019-12-04 17:14:30 -0500

    refactor: ApptCard production styling

--- src/components/ApptCard.js
+++ src/components/ApptCard.js
@@ -1,41 +1,35 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 import './ApptCard.scss';
-import { getFriendlyActionType, getApptDate, getHourString } from '../utils';
+import { ReactComponent as ChevronRight } from '../images/chevron-right-solid.svg';
+import { getFriendlyActionType, getApptDate } from '../utils';
 
 const ApptCardAction = ({ action, isCustomer }) => {
   const title = getFriendlyActionType(action.type, isCustomer ? 'CUSTOMER' : 'ADMIN');
 
   return (
     <div className={`appt-card__action appt-card__action--${action.type}`}>
-      {/* <ActionIcon title={title} actionType={action.type} /> */}
       {title} {action.containerId && <span className="appt-card__action__cid">({action.containerId})</span>}
     </div>
   );
 }
 
 const ApptCard = ({ appt, onClick, isCustomer }) => (
-  <div>
-    <div className="appt-card" onClick={onClick}>
-      <div className="appt-card__date">{getApptDate(appt).toDateString()} {!isCustomer && '— ' + getHourString(getApptDate(appt).getHours())}</div>
-
-      {!isCustomer && (
-        <div>
-          <div><span className="appt-card__field-name">ID ·</span> {appt.id}</div>
-          <div><span className="appt-card__field-name">CUSTOMER ·</span> {appt.user.name} ({appt.user.company})</div>
-        </div>
-      )}
-
-      <div>
-        <span className="appt-card__field-name">{isCustomer ? 'ARRIVE AT' : 'ARRIVAL WINDOW'} ·</span> {appt.arrivalWindow}
-      </div>
-
-      {appt.comment && <div><span className="appt-card__field-name">COMMENT ·</span> {appt.comment}</div>}
-
+  <div className="appt-card" onClick={onClick}>
+    <div className="appt-card-details">
+      <div className="appt-card__id">{appt.id}</div>
+      {!isCustomer && <div className="appt-card__customer">{appt.user.name} <span className="appt-card__company">({appt.user.company})</span></div>}
+      <div className="appt-card__date">{!isCustomer ? format(getApptDate(appt), `MMM d, yyyy '—' HH:mm`) : format(getApptDate(appt), `iii, MMM d, yyyy`)}</div>
+      <div className="appt-card__arrival-window">{appt.arrivalWindow}</div>
       <div className="appt-card__actions-list">
         {appt.actions.map((action, i) => <ApptCardAction key={i} action={action} isCustomer={isCustomer} />)}
       </div>
     </div>
+
+    <button className="appt-card__view-details-button" type="button" onClick={onClick}>
+      View Details <ChevronRight />
+    </button>
   </div>
 );
 
